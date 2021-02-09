e6ba4d4c96fbd157ef920cf0d5c78a19600dab47
Aleksey Vedin <lumenlawliet@gmail.com>
2021-02-09 09:09:10 +0400

    lesson3_step4 parametrize

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,11 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
     REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
+
+class ProductPageLocators():
+    ADD_PRODUCT_TO_BASKET_BUTTON = (By.CSS_SELECTOR, '#add_to_basket_form')
+    PRODUCT_NAME = (By.CSS_SELECTOR, '.col-sm-6.product_main>h1')
+    PRODUCT_PRICE = (By.CSS_SELECTOR, '.price_color')
+    ALERT_ADDED_PRODUCT = (By.CSS_SELECTOR, '#messages >.fade.in:nth-child(1)>.alertinner')
+    PRODUCT_NAME_IN_MESSAGE = (By.CSS_SELECTOR, '#messages >.fade.in:nth-child(1)>.alertinner>strong')
+    PRODUCT_PRICE_IN_BASKET = (By.CSS_SELECTOR, '.alertinner > p > strong')
