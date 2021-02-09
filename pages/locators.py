66c7585a71b94abaf0083235ebd93567394334fc
Aleksey Vedin <lumenlawliet@gmail.com>
2021-02-09 12:44:41 +0400

    add test_guest_should_see_login_link_on_product_page and test_guest_can_go_to_login_page_from_product_page refactor main page and base page

--- pages/locators.py
+++ pages/locators.py
@@ -1,9 +1,7 @@
 from selenium.webdriver.common.by import By
 
-
-class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
-
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
@@ -13,7 +11,7 @@ class ProductPageLocators():
     ADD_PRODUCT_TO_BASKET_BUTTON = (By.CSS_SELECTOR, '#add_to_basket_form')
     PRODUCT_NAME = (By.CSS_SELECTOR, '.col-sm-6.product_main>h1')
     PRODUCT_PRICE = (By.CSS_SELECTOR, '.price_color')
-    ALERT_ADDED_PRODUCT = (By.CSS_SELECTOR, '#messages >.fade.in:nth-child(1)>.alertinner')
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, '#messages >.fade.in:nth-child(1)>.alertinner')
     PRODUCT_NAME_IN_MESSAGE = (By.CSS_SELECTOR, '#messages >.fade.in:nth-child(1)>.alertinner>strong')
     PRODUCT_PRICE_IN_BASKET = (By.CSS_SELECTOR, '.alertinner > p > strong')
     ALERT_PRICE_MESSAGES = (By.CSS_SELECTOR, '.alert-info.fade.in> .alertinner')
--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,10 @@
 from .pages.login_page import LoginPage
-from .pages.main_page import MainPage
+from .pages.main_page import BasePage
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com"
-    page = MainPage(browser, link)
+    page = BasePage(browser, link)
     page.open()
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
@@ -13,7 +13,8 @@ def test_guest_can_go_to_login_page(browser):
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+    page = BasePage(browser, link)
     page.open()
-    page.go_to_login_page()
     page.should_be_login_link()
+
+
